f4ee58f9af9097d540833f2a0b02d096d3302bb3
Mylena Vitoriano <mylenabarbosa08@gmail.com>
2022-06-20 17:10:09 -0300

    iniciado processo de filtragem de pokemons de acordo com seus tipos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import { Pokemons } from './components/Pokemons'
+import { useState, useEffect } from 'react'
 import './global.scss'
 
 export default function App() {
+
+  const [allTypes, setAllTypes] = useState([])
+  const [loadMoreTypes, setLoadMoreTypes] = useState('https://pokeapi.co/api/v2/type')
+
+  const [type, setType] = useState('all')
+
+  const loadTypes = async () => {
+    const res = await fetch(loadMoreTypes)
+    const data = await res.json()
+
+    setLoadMoreTypes(data.next)
+
+    const createTypeObject = (result) =>{
+        result.forEach( async (type) => {
+            if(type.name != 'unknown' & type.name != 'shadow'){
+                const res = await fetch(`${loadMoreTypes}/${type.name}`)
+                const data = await res.json()
+
+                setAllTypes(currentList => [...currentList, data])
+            }
+        })
+    }
+    createTypeObject(data.results)
+}
+
+useEffect(() => {
+    loadTypes()
+}, [])
+
+function selectType(typeName){
+  setType(typeName)
+}
 
   return(
     <main>
@@ -15,7 +48,24 @@ export default function App() {
         </div>
       </div>
 
-      <Pokemons/>
+      <div className="conteudo">
+        <div className='types'>
+          <button className='type' onClick={() => selectType(`all`)}>
+              <img src="src/assets/icons/icon-types/all-pokemons.svg" alt="" />
+              <p className='type-all-pokemons'>All</p>
+          </button>
+
+          {allTypes.map((item, index) => (
+              <button className='type' key={index} onClick={() => selectType(item.name)}>
+                  <img src={`src/assets/icons/icon-types/${item.name}.svg`} alt="" />
+                  <p className={`type-${item.name}`}>{item.name[0].toUpperCase() + item.name.substring(1)}</p>
+              </button>
+          ))}
+        </div>
+
+        <Pokemons type={type}/>
+      </div>
+
     </main>
   )
 }
--- src/components/Pokemons/index.jsx
+++ src/components/Pokemons/index.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
-import { render } from 'react-dom'
 import './styles.scss'
 
-export function Pokemons(){
+export function Pokemons({type}){
 
     const [allPokemons, setAllPokemons] = useState([])
     const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=20')
+    const [control, setControl] = useState(1)
 
+    let maxPokemons = 20
+
+    
     const loadPokemons = async () => {
         const res = await fetch(loadMore)
         const data = await res.json()
@@ -14,24 +17,52 @@ export function Pokemons(){
         setLoadMore(data.next)
 
         const createPokemonObject = (result) =>{
-            result.forEach( async (pokemon) => {
-                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+            result.forEach( async (poke) => {
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke.name}`)
                 const data = await res.json()
-
+                
                 setAllPokemons(currentList => [...currentList, data])
             })
         }
+
         createPokemonObject(data.results)
     }
 
+    const loadPokemonsType = async () => {
+        const res = await fetch(`https://pokeapi.co/api/v2/type/${type}`)
+        const data = await res.json()
+
+        const createPokemonObject = (result) =>{
+            console.log(20 * control)
+            for(let i = ((control - 1) * maxPokemons); i < (20 * control); i++){
+                fetch(`https://pokeapi.co/api/v2/pokemon/${result[i].pokemon.name}`)
+                .then( res => res.json())
+                .then(data => setAllPokemons(currentList => [...currentList, data]))
+            }
+        }
+        setAllPokemons('')
+        createPokemonObject(data.pokemon)
+    }
+
     useEffect(() => {
-        loadPokemons()
-    }, [])
+        if(type != 'all'){
+            loadPokemonsType()
+            console.log(allPokemons)
+        }else{
+            loadPokemons()
+            console.log(allPokemons)
+        }
+
+        setAllPokemons(currentList => [ ...currentList.slice(0, -1)])
+        console.log("AQUIIII")
+    }, [type])
 
     return(
         <div className='pokemons'>
-            {allPokemons.map((item, index) => (
+            {allPokemons != '' ? allPokemons.map((item, index) => (
+                item.sprites.other.home.front_default &&
                 <div className={`pokemon ${item.types[0].type.name}`} key={index}>
+                    
                     <div className='poke-description'>
                         <p>#{item.id < 10 ? '00'+item.id : '0'+item.id}</p>
                         <h2>{item.name[0].toUpperCase() + item.name.substring(1)}</h2>
@@ -43,12 +74,12 @@ export function Pokemons(){
                         </div>
                     </div>
                     <div className='poke-image'>
-                        <img src={item.sprites.other.dream_world.front_default} alt="" />
+                        <img src={item.sprites.other.home.front_default} alt="" />
                     </div>
                 </div>
-            ))}
+            )) : ''}
 
-            <button className='btn-load-more' onClick={() => loadPokemons()}>Load More</button>
+            <button className='btn-load-more' onClick={type != "all" ? () => {loadPokemonsType(); setControl(control + 1)}  : () => loadPokemons()}>Load More</button>
         </div>
     )
     
